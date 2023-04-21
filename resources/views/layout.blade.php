f1856e49f63e7287952a5bf0ec2d5312a7e4f631
FranciaMMI <francia.bidzimou@mmibordeaux.com>
2023-04-21 12:19:26 +0200

    add layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=$, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     @yield('content')
