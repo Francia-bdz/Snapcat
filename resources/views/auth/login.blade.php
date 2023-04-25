97b0334027a4e802b0a11d5226e91936700e80b1
FranciaMMI <francia.bidzimou@mmibordeaux.com>
2023-04-25 09:39:32 +0200

    add style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                                     <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                                     <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror border" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -28,7 +28,7 @@
                                     <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                                     <div class="col-md-6">
-                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror border" name="password" required autocomplete="current-password">
 
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,15 @@
 @extends('layout')
     @section('content')
-         <h1> Coucou {{ Auth::user()->name }}, bienvenue sur Snapcat </h1>
+
+    <div class="p-2 h-screen bg-emerald-100">
+         <h1 class="font-extrabold text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"> Coucou {{ Auth::user()->name }} ! Bienvenue sur Snapcat </h1>
          <form action="{{ route('logout') }}" method="post">
             @csrf
-            <button>Déconnexion</button>
+            <button class="my-6 bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full" >Déconnexion</button>
         </form>
         <div class="">
-           <a href="{{ route('users.index') }}"> Voir les utilisateurs </a>
-  
+           <a href="{{ route('users.index') }}" class="text-sky-500 hover:text-sky-700 underline underline-offset-4 font-bold"> Voir les utilisateurs </a>
         </div>
+    </div>
+
     @endsection
